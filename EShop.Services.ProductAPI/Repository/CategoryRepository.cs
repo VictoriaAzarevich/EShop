026084d5d8507b0cde13d4556ed4048392c3ba44
Victoria Azarevich <azarevich.victoria@gmail.com>
2025-02-18 16:18:06 +0300

    Add CategoryRepository

---
+++ EShop.Services.ProductAPI/Repository/CategoryRepository.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using EShop.Services.ProductAPI.DbContexts;
+using EShop.Services.ProductAPI.Models;
+using EShop.Services.ProductAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Services.ProductAPI.Repository
+{
+    public class CategoryRepository(ApplicationDbContext dbContext, IMapper mapper) : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _dbContext = dbContext;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<CategoryDto> CreateCategory(CategoryCreateUpdateDto categoryCreateUpdateDto)
+        {
+            var category = _mapper.Map<Category>(categoryCreateUpdateDto);
+
+            _dbContext.Categories.Add(category);
+            await _dbContext.SaveChangesAsync();
+
+            return _mapper.Map<CategoryDto>(category);
+        }
+
+        public async Task DeleteCategory(int categoryId)
+        {
+            var category = await _dbContext.Categories
+                .FirstOrDefaultAsync(p => p.CategoryId == categoryId);
+
+            if (category == null)
+            {
+                throw new KeyNotFoundException("Category not found");
+            }
+
+            _dbContext.Categories.Remove(category);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<CategoryDto>> GetCategories()
+        {
+            var categories = await _dbContext.Categories.ToListAsync();
+            return _mapper.Map<IEnumerable<CategoryDto>>(categories);
+        }
+
+        public async Task<CategoryDto> GetCategoryById(int categoryId)
+        {
+            var category = await _dbContext.Categories
+                .FirstOrDefaultAsync(p => p.CategoryId == categoryId);
+
+            if (category == null)
+            {
+                throw new KeyNotFoundException("Category not found");
+            }
+
+            return _mapper.Map<CategoryDto>(category);
+        }
+
+        public async Task<CategoryDto> UpdateCategory(int categoryId, CategoryCreateUpdateDto categoryCreateUpdateDto)
+        {
+            var category = await _dbContext.Categories
+                .FirstOrDefaultAsync(p => p.CategoryId == categoryId);
+
+            if (category == null)
+            {
+                throw new KeyNotFoundException("Category not found");
+            }
+
+            _mapper.Map(categoryCreateUpdateDto, category);
+            await _dbContext.SaveChangesAsync();
+
+            return _mapper.Map<CategoryDto>(category);
+        }
+    }
+}
---
+++ EShop.Services.ProductAPI/Repository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using EShop.Services.ProductAPI.Models.Dto;
+
+namespace EShop.Services.ProductAPI.Repository
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<CategoryDto>> GetCategories();
+        Task<CategoryDto> GetCategoryById(int categoryId);
+        Task<CategoryDto> CreateCategory(CategoryCreateUpdateDto categoryCreateUpdateDto);
+        Task<CategoryDto> UpdateCategory(int categoryId, CategoryCreateUpdateDto categoryCreateUpdateDto);
+        Task DeleteCategory(int categoryId);
+    }
+}
