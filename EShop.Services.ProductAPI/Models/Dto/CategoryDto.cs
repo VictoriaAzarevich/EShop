3571b25824ae6f77e5d1fde8e34da539ae815867
Victoria Azarevich <azarevich.victoria@gmail.com>
2025-02-18 14:10:59 +0300

    Add dto

---
+++ EShop.Services.ProductAPI/Models/Dto/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EShop.Services.ProductAPI.Models.Dto
+{
+    public class CategoryDto
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
