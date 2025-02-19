16db64d78c7ad5c74a968f57f1c3093f4c0f35db
Victoria Azarevich <azarevich.victoria@gmail.com>
2025-02-19 17:44:05 +0300

    Configure Cloudinary

--- EShop.Services.ProductAPI/Program.cs
+++ EShop.Services.ProductAPI/Program.cs
@@ -1,11 +1,21 @@
+using CloudinaryDotNet;
+using EShop.Services.ProductAPI;
 using EShop.Services.ProductAPI.DbContexts;
 using EShop.Services.ProductAPI.Repository;
 using EShop.Services.ProductAPI.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
+builder.Services.AddSingleton(provider =>
+{
+    var config = provider.GetRequiredService<IOptions<CloudinarySettings>>().Value;
+    var account = new Account(config.CloudName, config.ApiKey, config.ApiSecret);
+    return new Cloudinary(account);
+});
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
