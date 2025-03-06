164269be2d43d534859d1835e0d5bfac015ca0fd
Victoria Azarevich <vmisyakova@gmail.com>
2025-03-06 18:46:33 +0300

    Add models and create DB

--- ProductAPI/DBContext/ApplicationDbContext.cs
+++ ProductAPI/DBContext/ApplicationDbContext.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Models;
 
 namespace ProductAPI.DBContext
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
---
+++ ProductAPI/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductAPI.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public required string Name { get; set; }
+        public double Price { get; set; }
+        public string? Description { get; set; }
+        public int CategoryId { get; set; }
+        public required Category Category { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
