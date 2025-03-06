0d77f7f8e8257e53f36b2f0873c7c89480650221
Victoria Azarevich <vmisyakova@gmail.com>
2025-03-06 19:02:00 +0300

    Configure AutoMapper

---
+++ ProductAPI/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using ProductAPI.Models.Dto;
+using ProductAPI.Models;
+using AutoMapper;
+
+namespace ProductAPI
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, ProductDto>()
+                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(scr => scr.Category.CategoryName));
+            CreateMap<ProductCreateUpdateDto, Product>();
+            CreateMap<Category, CategoryDto>();
+            CreateMap<CategoryCreateUpdateDto, Category>();
+        }
+    }
+}
