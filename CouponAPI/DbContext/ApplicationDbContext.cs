fddf666d973440cd2cdf051a9d3d124ca0e0d2b3
Victoria Azarevich <vmisyakova@gmail.com>
2025-03-17 13:00:03 +0300

    Configure DbContext

---
+++ CouponAPI/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CouponAPI.DBContext
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+    }
+}
