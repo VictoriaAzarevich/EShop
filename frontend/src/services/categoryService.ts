631ed0c7d369b4d0005eb8cc7d74db626ece1f87
Victoria Azarevich <vmisyakova@gmail.com>
2025-04-28 21:17:39 +0300

    Add api requests to manage categories

--- frontend/src/services/categoryService.ts
+++ frontend/src/services/categoryService.ts
@@ -1,9 +1,27 @@
 import axios from "axios";
 import { CategoryResponse } from "../types/CategoryResponse";
+import { CategoryRequest} from "../types/CategoryRequest";
 
-const CATEGORY_URL = "https://localhost:7230/api/category";
+const BASE_URL = "https://localhost:7230/api/category";
 
 export const getCategories = async (): Promise<CategoryResponse[]> => {
-  const response = await axios.get(CATEGORY_URL);
+  const response = await axios.get(BASE_URL);
   return response.data;
+};
+
+export const getCategoryById = async (id: number): Promise<CategoryResponse> => {
+  const response = await axios.get(`${BASE_URL}/${id}`);
+  return response.data;
+};
+
+export const createCategory = async (category: CategoryRequest): Promise<void> => {
+  await axios.post(`${BASE_URL}`, category);
+};
+
+export const updateCategory = async (category: CategoryRequest): Promise<void> => {
+  await axios.put(`${BASE_URL}`, { category });
+};
+
+export const deleteCategory = async (id: number): Promise<void> => {
+  await axios.delete(`${BASE_URL}/${id}`);
 };
