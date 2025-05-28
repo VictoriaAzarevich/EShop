acdede8b1e3371440f0cb58b308f37572bd26c53
Victoria Azarevich <vmisyakova@gmail.com>
2025-05-28 22:27:32 +0300

    DbContext Configuration

---
+++ EShop.OrderAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using EShop.OrderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EShop.OrderAPI.DbContexts
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+    }
+}
