f14a7551b88da7abc954cd49149b3cc71543305f
Victoria Azarevich <vmisyakova@gmail.com>
2025-07-12 16:18:42 +0300

    Configure receiving order confirmation messages

---
+++ EShop.Email/Messaging/PaymentConsumer.cs
@@ -0,0 +1,26 @@
+﻿using EShop.Contracts;
+using EShop.Email.Repository;
+using MassTransit;
+
+namespace EShop.Email.Messaging
+{
+    public class PaymentConsumer(IEmailRepository emailRepository, ILogger<PaymentConsumer> logger) : IConsumer<IUpdatePaymentResultMessage>
+    {
+        private readonly IEmailRepository _emailRepository = emailRepository;
+        private readonly ILogger<PaymentConsumer> _logger = logger;
+
+        public async Task Consume(ConsumeContext<IUpdatePaymentResultMessage> context)
+        {
+            var message = context.Message;
+
+            try
+            {
+                await _emailRepository.SendAndLogEmail(message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error sending email for payment update message with Id: {MessageId}", message.Id);
+            }
+        }
+    }
+}
--- EShop.Email/Program.cs
+++ EShop.Email/Program.cs
@@ -1,4 +1,7 @@
 using EShop.Email.DbContexts;
+using EShop.Email.Messaging;
+using EShop.Email.Repository;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +9,32 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IEmailRepository, EmailRepository>();
+
+var rabbitConfig = builder.Configuration.GetSection("RabbitMQ");
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<PaymentConsumer>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(
+            rabbitConfig["Host"],
+            rabbitConfig["VirtualHost"],
+            h =>
+            {
+                h.Username(rabbitConfig["Username"]);
+                h.Password(rabbitConfig["Password"]);
+            });
+
+        cfg.ReceiveEndpoint(rabbitConfig["EmailQueue"], e =>
+        {
+            e.ConfigureConsumer<PaymentConsumer>(context);
+            e.Bind(rabbitConfig["PaymentExchange"]);
+        });
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
