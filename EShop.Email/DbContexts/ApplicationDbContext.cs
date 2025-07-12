0f39989f85875d2f2cf744e6193e66b47adf6909
Victoria Azarevich <vmisyakova@gmail.com>
2025-07-12 15:42:38 +0300

    Configure a database for sending messages

---
+++ EShop.Email/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using EShop.Email.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Email.DbContexts
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<EmailLog> EmailLogs { get; set; }
+    }
+}
