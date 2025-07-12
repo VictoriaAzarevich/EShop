46d52d09f61a02b21ea582ffe95f43ab657a604a
Victoria Azarevich <vmisyakova@gmail.com>
2025-07-12 15:43:04 +0300

    Create an email repository

---
+++ EShop.Email/Repository/EmailRepository.cs
@@ -0,0 +1,25 @@
+﻿using EShop.Contracts;
+using EShop.Email.DbContexts;
+using EShop.Email.Models;
+
+namespace EShop.Email.Repository
+{
+    public class EmailRepository(ApplicationDbContext dbContext) : IEmailRepository
+    {
+        private readonly ApplicationDbContext _dbContext = dbContext;
+
+        public async Task SendAndLogEmail(IUpdatePaymentResultMessage message)
+        {
+            // Implement an email sender
+            EmailLog emailLog = new EmailLog()
+            {
+                Email = message.Email,
+                EmailSent = DateTime.Now,
+                Log = $"Order - {message.OrderId} has been created successfully."
+            };
+
+            _dbContext.Add(emailLog);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ EShop.Email/Repository/IEmailRepository.cs
@@ -0,0 +1,9 @@
+﻿using EShop.Contracts;
+
+namespace EShop.Email.Repository
+{
+    public interface IEmailRepository
+    {
+        Task SendAndLogEmail(IUpdatePaymentResultMessage message);
+    }
+}
